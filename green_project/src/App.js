471740cfdc79e998e9175e045fdb1d8bbc9ca151
Ali Sanad <alisanad099@gmail.com>
2021-07-19 05:08:27 +0200

    Contact Us , Clients and Footer

--- green_project/src/App.js
+++ green_project/src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import Header from "./components/header/header";
-import NavBar from "./components/navbar";
+import React from 'react';
+import Clients from './components/Clients';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import Header from './components/header/header';
+import NavBar from './components/navbar';
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Header />
+      <ContactUs />
+      <Clients />
+      <Footer />
     </>
   );
 };
